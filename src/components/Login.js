33273825955d0b5346c49532cb61956b4682d686
Vijay <patilvijaysing2092@gmail.com>
2023-11-13 20:42:47 +0530

    done2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,44 @@
 import React, { useState } from 'react';
 
+const Login = ({ isLoggedIn, setIsLoggedIn }) => {
+  const [data, setData] = useState({ username: '', password: '' });
 
-const Login = ({isLoggedIn, setIsLoggedIn}) =>{
-    const [data, setData] = useState({username :'', password:''});
+  const handleChange = (e) => {
+    console.log(e.target, e.target.name);
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-
-    const handleChage =(e)=>{
-        console.log(e.target, e.target.name);
-        setData({...data,[e.target.name]: e.target.value});
-
-    };
-
-    return (
+  return (
     <div>
-        {isLoggedIn ? (<p>Your are logged in!</p>) : (
-         <form>
-            <div>
-                <lable htmlFor ="username">Username:</lable>
-                <input 
-                type="text"
-                 id='username' 
-                 name ='username'  
-                 value={data.username} 
-                 onChange={handleChage}/>
-            </div>
-            <div>
-                <lable htmlFor ="password">Password:</lable>
-                <input 
-                type="password" 
-                id='password' 
-                name ='password' 
-                value={data.password}
-                onChange={handleChage} />
-            </div>
-            <button onClick={() => setIsLoggedIn(true)}>
-                Login
-            </button>
-         </form>
-        )}
-       
+      {isLoggedIn ? (
+        <p>You are logged in!</p>
+      ) : (
+        <form>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={data.username}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={data.password}
+              onChange={handleChange}
+            />
+          </div>
+          <button onClick={() => setIsLoggedIn(true)}>Login</button>
+        </form>
+      )}
     </div>
-    );
+  );
 };
-
 
 export default Login;
